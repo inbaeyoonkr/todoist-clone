77c521c1b32009d052cce8bb4d3643e7549e1d2a
ringo2455 <iby2455@gmail.com>
2019-09-02 16:43:36 +0900

    Projects, SelectedProject context 추가

---
+++ src/context/projects-context.js
@@ -0,0 +1,16 @@
+import React, { createContext, useContext } from 'react';
+import { useProjects } from '../hooks';
+
+export const ProjectsContext = createContext();
+// ProjectsProvider는 Context에서 사용할 값을 설정할 때 사용한다.
+export const ProjectsProvider = ({ children }) => {
+  const { projects, setProjects } = useProjects();
+
+  return (
+    <ProjectsContext.Provider value={{ projects, setProjects }}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export const useProjectsValue = () => useContext(ProjectsContext);
